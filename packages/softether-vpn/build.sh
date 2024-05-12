3b5b0b129ee625043497c8c595bbcec2d8ed438a
Twaik Yont <twaikyont@gmail.com>
2024-05-12 18:56:36 +0300

    bump(main/softether-vpn): 5.02.5184

--- packages/softether-vpn/build.sh
+++ packages/softether-vpn/build.sh
@@ -2,13 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://www.softether.org/
 TERMUX_PKG_DESCRIPTION="An open-source cross-platform multi-protocol VPN program"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=(5.02.5180)
+TERMUX_PKG_VERSION=(5.02.5184)
 TERMUX_PKG_REVISION=4
-TERMUX_PKG_VERSION+=(1.0.18)
+TERMUX_PKG_VERSION+=(1.0.19)
 TERMUX_PKG_SRCURL=(https://github.com/SoftEtherVPN/SoftEtherVPN/releases/download/${TERMUX_PKG_VERSION}/SoftEtherVPN-${TERMUX_PKG_VERSION}.tar.xz
                    https://github.com/jedisct1/libsodium/archive/${TERMUX_PKG_VERSION[1]}-RELEASE.tar.gz)
-TERMUX_PKG_SHA256=(b5649a8ea3cc6477325e09e2248ef708d434ee3b2251eb8764bcfc15fb1de456
-                   b7292dd1da67a049c8e78415cd498ec138d194cfdb302e716b08d26b80fecc10)
+TERMUX_PKG_SHA256=(7459f321ec957d160f95ccf5fccc46be6f2c26bd78f0bcdf03d53ae131d051f5
+                   4fb996013283f482f46a457c8ff2c1495e797788e78e8ec56b1aa1b19253bf75)
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="libiconv, libsodium, ncurses, openssl, readline, resolv-conf, zlib"
 TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -22,6 +23,7 @@ termux_step_post_get_source() {
 }
 
 termux_step_host_build() {
+	LDFLAGS+=" -lm"
 	local _PREFIX_FOR_BUILD=$TERMUX_PKG_HOSTBUILD_DIR/prefix
 	mkdir -p $_PREFIX_FOR_BUILD
 	mkdir -p libsodium
